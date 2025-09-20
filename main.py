bff64a0c3b47dbbdbdb8e4f1211dcbf5d829db3d
Miguel Alves <migueldealmeidaalves55@gmail.com>
2025-09-20 16:10:34 -0300

    now it has a window, uaaah

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import pygame
+from constants import *
+
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(000000, rect=None, special_flags=0)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
